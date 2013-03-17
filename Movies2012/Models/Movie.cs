95799772f739c167c54643090ef32556373bdd2c
shiva <shivagnank@gmail.com>
2013-03-17 13:46:24 +0800

    Movies2012 base project

--- Movies2012/Models/Movie.cs
+++ Movies2012/Models/Movie.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Movies2012.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public DateTime ReleaseDate { get; set; }
+        [Required]
         public string Genre { get; set; }
+        [Required]
         public decimal Price { get; set; }
     }
 
