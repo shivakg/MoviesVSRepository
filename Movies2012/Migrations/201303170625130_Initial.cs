8b5868687c14958fe3bae00dd8a1ce964b16d91e
shiva <shivagnank@gmail.com>
2013-03-17 14:27:54 +0800

    Migrations included

---
+++ Movies2012/Migrations/201303170625130_Initial.cs
@@ -0,0 +1,20 @@
+namespace Movies2012.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Movies", "Title", c => c.String(nullable: false));
+            AlterColumn("dbo.Movies", "Genre", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Movies", "Genre", c => c.String());
+            AlterColumn("dbo.Movies", "Title", c => c.String());
+        }
+    }
+}
